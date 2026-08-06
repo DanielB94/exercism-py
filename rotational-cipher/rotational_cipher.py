5b190a94be8c900cf1499517c75dfd22d1218a94
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2026-08-06 10:38:35 -0500

    I deleted line 7 an set the result of the math directly in cipeherd_text var

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -4,8 +4,7 @@ def rotate(text, key):
     for character in text:
         if character.isalpha():
             base = ord("A") if character.isupper() else ord("a")
-            ciphered_letter = chr((ord(character) - base + key) % 26 + base)
-            ciphered_text += ciphered_letter
+            ciphered_text += chr((ord(character) - base + key) % 26 + base)
         else:
             ciphered_text += character
     return ciphered_text
